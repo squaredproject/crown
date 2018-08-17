f7624ec0295068b0d7236e3c768e9ef73296d5c0
squaredproject <squaredproject@icloud.com>
2018-08-16 21:01:07 -0700

    made run command work on odroid

--- Crown/run.sh
+++ Crown/run.sh
@@ -2,4 +2,4 @@
 
 cd "$( dirname "$0" )"
 echo java -Xms256m -Xmx1g -cp "build/classes/main:code/*" RunHeadless "${PWD}"
-exec java -Xms256m -Xmx1g -cp "build/classes/java/main:code/*" RunHeadless "${PWD}"
+exec java -Xms256m -Xmx1g -cp "build/classes/main:code/*" RunHeadless "${PWD}"
